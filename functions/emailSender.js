72ea21037ef2a7afd4fc04c11938c0aa22298dfa
steingruberbastian <steingruberbastian@gmail.com>
2024-12-23 21:40:03 +0100

    1.0 bugfix

--- functions/emailSender.js
+++ functions/emailSender.js
@@ -7,14 +7,10 @@ const GOOGLE_EMAIL_PASSWORD = process.env.GOOGLE_EMAIL_PASSWORD;
 
 export async function sendEmail() {
 
-  console.log("GOOGLE_EMAIL:", process.env.GOOGLE_EMAIL);
-  console.log("GOOGLE_EMAIL_PASSWORD:", process.env.GOOGLE_EMAIL_PASSWORD);
-
-
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // Use true for port 465, false for 587
+    port: 587,
+    secure: false, // Use true for port 465, false for 587
     auth: {
       user: GOOGLE_EMAIL,
       pass: GOOGLE_EMAIL_PASSWORD,
