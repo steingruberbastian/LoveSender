6b7e71f8ee2b20a4f5e9123731e243015533bd24
steingruberbastian <steingruberbastian@gmail.com>
2024-12-23 21:09:16 +0100

    1.0 bugfix

--- src/lib/emailSender.server.js
+++ src/lib/emailSender.server.js
@@ -1,5 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import nodemailer from "nodemailer";
-import { GOOGLE_EMAIL, GOOGLE_EMAIL_PASSWORD } from "$env/static/private";
+const GOOGLE_EMAIL = process.env.GOOGLE_EMAIL;
+const GOOGLE_EMAIL_PASSWORD = process.env.GOOGLE_EMAIL_PASSWORD;
 
 export async function sendEmail() {
   let transporter = nodemailer.createTransport({
