ed0c71346441fcf8d48afc987a6ea50f3afe20c0
steingruberbastian <steingruberbastian@gmail.com>
2025-01-06 14:48:03 +0100

    fixed year filter bug + added image for next holidays memory

--- src/routes/menue/memories/create/+page.server.js
+++ src/routes/menue/memories/create/+page.server.js
@@ -8,22 +8,12 @@ export async function load() {
 export const actions = {
     create: async ({ request }) => {
         let data = await request.formData();
-        let categoryValue = data.get('category');
-        let category;
-
-        if (categoryValue === 'Ferien') {
-            category = 1;
-        } else if (categoryValue === 'Zuhause') {
-            category = 2;
-        } else {
-            category = 3;
-        }
 
         let memory = {
             title: data.get('title'),
             location: data.get('location'),
-            year: data.get('year'),
-            category_id: category
+            year: parseInt(data.get('year')),
+            category_id: parseInt(data.get('category'))
         };
 
         await db.createMemory(memory);
