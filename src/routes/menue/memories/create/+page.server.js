31fa2f0bf029efa30a52ff0a36e84a1f71b7b6db
steingruberbastian <steingruberbastian@gmail.com>
2025-01-07 16:03:41 +0100

    added debug code

--- src/routes/menue/memories/create/+page.server.js
+++ src/routes/menue/memories/create/+page.server.js
@@ -7,29 +7,33 @@ export async function load() {
 
 export const actions = {
     create: async ({ request }) => {
-        const data = await request.formData();
-        const imageFile = data.get('image');
-        let setMemory = false;
-        if (data.get('memory') === 'on') {
-            setMemory = true;
+        try {
+            const data = await request.formData();
+            const imageFile = data.get('image');
+            let setMemory = false;
+            if (data.get('memory') === 'on') {
+                setMemory = true;
+            }
+    
+            let imagePath = '/images/placeholder.jpg';
+    
+            if (imageFile && imageFile.size > 0) {
+                const buffer = Buffer.from(await imageFile.arrayBuffer());
+                imagePath = `data:${imageFile.type};base64,${buffer.toString('base64')}`;
+            }
+    
+            let memory = {
+                title: data.get('title'),
+                location: data.get('location'),
+                year: parseInt(data.get('year')),
+                image: imagePath,
+                memory: setMemory,
+                category_id: parseInt(data.get('category'))
+            };
+    
+            await db.createMemory(memory);
+        } catch(err) {
+            console.error(err);
         }
-
-        let imagePath = '/images/placeholder.jpg';
-
-        if (imageFile && imageFile.size > 0) {
-            const buffer = Buffer.from(await imageFile.arrayBuffer());
-            imagePath = `data:${imageFile.type};base64,${buffer.toString('base64')}`;
-        }
-
-        let memory = {
-            title: data.get('title'),
-            location: data.get('location'),
-            year: parseInt(data.get('year')),
-            image: imagePath,
-            memory: setMemory,
-            category_id: parseInt(data.get('category'))
-        };
-
-        await db.createMemory(memory);
     }
 }
